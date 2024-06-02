ae77d20c14673c932fc7b52adc843b584fe47482
brainwarpgit <kmilan@dyercs.net>
2024-06-01 21:10:58 -0500

    6-1-24 [SWGDL] Starting items removed from player creation and tutorial. Items moved to Helper droid. Helper droid quests xp and reward increased.
    Reworked XP given during Droid Quests.  Added better rewards for each profession's end quest.

--- MMOCoreORB/bin/scripts/managers/player_creation_manager.lua
+++ MMOCoreORB/bin/scripts/managers/player_creation_manager.lua
@@ -19,42 +19,16 @@ professions = {
 	"social_entertainer"
 }
 
-marksmanPistol = "object/weapon/ranged/pistol/pistol_cdef.iff"
-	
-marksmanRifle = "object/weapon/ranged/rifle/rifle_cdef.iff"
-
-marksmanCarbine = "object/weapon/ranged/carbine/carbine_cdef.iff"
-
-brawlerOneHander = "object/weapon/melee/knife/knife_stone.iff"
-
-brawlerTwoHander = "object/weapon/melee/axe/axe_heavy_duty.iff"
-
-brawlerPolearm = "object/weapon/melee/polearm/lance_staff_wood_s1.iff"
-
-survivalKnife = "object/weapon/melee/knife/knife_survival.iff"
-
-genericTool = "object/tangible/crafting/station/generic_tool.iff"
-
-foodTool = "object/tangible/crafting/station/food_tool.iff"
-
-mineralTool = "object/tangible/survey_tool/survey_tool_mineral.iff"
-
-chemicalTool = "object/tangible/survey_tool/survey_tool_liquid.iff"
-
-slitherhorn = "object/tangible/instrument/slitherhorn.iff"
-
 marojMelon = "object/tangible/food/foraged/foraged_fruit_s1.iff"
 
-x31Speeder = "object/tangible/deed/vehicle_deed/landspeeder_x31_deed.iff"
-
 professionSpecificItems = {
-	combat_brawler = { brawlerOneHander, brawlerTwoHander, brawlerPolearm },
-	combat_marksman = { marksmanPistol, marksmanCarbine, marksmanRifle },
-	crafting_artisan = { genericTool, mineralTool, chemicalTool },
+	combat_brawler = { },
+	combat_marksman = { },
+	crafting_artisan = { },
 	jedi = { },
-	outdoors_scout = { genericTool },
-	science_medic = { foodTool },
-	social_entertainer = { slitherhorn }
+	outdoors_scout = { },
+	science_medic = { },
+	social_entertainer = { }
 }
 
-commonStartingItems = { marojMelon, survivalKnife, x31Speeder }
+commonStartingItems = { marojMelon }
