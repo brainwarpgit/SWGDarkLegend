41067da8a1ea2dc60df41d369300326ef07b457d
brainwarpgit <kmilan@dyercs.net>
2024-07-10 07:26:48 -0500

    7-10-24 [SWGDL] Corrected armor component loot groups / updated loot_manager.lua

--- MMOCoreORB/bin/scripts/loot/groups/component_loot/armor_component.lua
+++ MMOCoreORB/bin/scripts/loot/groups/component_loot/armor_component.lua
@@ -5,15 +5,16 @@ armor_component = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "armor_segment_bone", weight = 1111112},
-		{itemTemplate = "armor_segment_chitin", weight = 1111111},
-		{itemTemplate = "armor_segment_composite", weight = 1111111},
-		{itemTemplate = "armor_segment_kashyyykian_black_mtn", weight = 1111111},
-		{itemTemplate = "armor_segment_kashyyykian_ceremonial", weight = 1111111},
-		{itemTemplate = "armor_segment_kashyyykian_hunting", weight = 1111111},
-		{itemTemplate = "armor_segment_padded", weight = 1111111},
-		{itemTemplate = "armor_segment_ubese", weight = 1111111},
-		{itemTemplate = "armor_segment_zam", weight = 1111111}
+		{itemTemplate = "krayt_composite_segment", weight = 10000000}
+--		{itemTemplate = "armor_segment_bone", weight = 1111112},
+--		{itemTemplate = "armor_segment_chitin", weight = 1111111},
+--		{itemTemplate = "armor_segment_composite", weight = 1111111},
+--		{itemTemplate = "armor_segment_kashyyykian_black_mtn", weight = 1111111},
+--		{itemTemplate = "armor_segment_kashyyykian_ceremonial", weight = 1111111},
+--		{itemTemplate = "armor_segment_kashyyykian_hunting", weight = 1111111},
+--		{itemTemplate = "armor_segment_padded", weight = 1111111},
+--		{itemTemplate = "armor_segment_ubese", weight = 1111111},
+--		{itemTemplate = "armor_segment_zam", weight = 1111111}
 	},
 }
 
--- MMOCoreORB/bin/scripts/loot/groups/component_loot/armor_component_advanced.lua
+++ MMOCoreORB/bin/scripts/loot/groups/component_loot/armor_component_advanced.lua
@@ -5,15 +5,16 @@ armor_component_advanced = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "armor_segment_bone_advanced", weight = 1111112},
-		{itemTemplate = "armor_segment_chitin_advanced", weight = 1111111},
-		{itemTemplate = "armor_segment_composite_advanced", weight = 1111111},
-		{itemTemplate = "armor_segment_kashyyykian_black_mtn_advanced", weight = 1111111},
-		{itemTemplate = "armor_segment_kashyyykian_ceremonial_advanced", weight = 1111111},
-		{itemTemplate = "armor_segment_kashyyykian_hunting_advanced", weight = 1111111},
-		{itemTemplate = "armor_segment_padded_advanced", weight = 1111111},
-		{itemTemplate = "armor_segment_ubese_advanced", weight = 1111111},
-		{itemTemplate = "armor_segment_zam_advanced", weight = 1111111}
+		{itemTemplate = "krayt_composite_segment", weight = 10000000}
+--		{itemTemplate = "armor_segment_bone_advanced", weight = 1111112},
+--		{itemTemplate = "armor_segment_chitin_advanced", weight = 1111111},
+--		{itemTemplate = "armor_segment_composite_advanced", weight = 1111111},
+--		{itemTemplate = "armor_segment_kashyyykian_black_mtn_advanced", weight = 1111111},
+--		{itemTemplate = "armor_segment_kashyyykian_ceremonial_advanced", weight = 1111111},
+--		{itemTemplate = "armor_segment_kashyyykian_hunting_advanced", weight = 1111111},
+--		{itemTemplate = "armor_segment_padded_advanced", weight = 1111111},
+--		{itemTemplate = "armor_segment_ubese_advanced", weight = 1111111},
+--		{itemTemplate = "armor_segment_zam_advanced", weight = 1111111}
 	},
 }
 
